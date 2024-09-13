837efcee2941eeef2a5ff1c5611838e4c97b1000
Sithum Imesh <sithum.imesh052002@icloud.com>
2024-09-13 11:51:05 +0530

    first commit

---
+++ back-end/back-end/src/test/java/com/wixis360/searchgas/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wixis360.searchgas.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
